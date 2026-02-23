46215ef3cd895f38f65165d1556c00cd123863be
SIC_04_ELWAS <107783540+Elwass@users.noreply.github.com>
2026-02-23 10:00:48 +0700

    Update Projects.jsx

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -4,7 +4,7 @@ import logo from "../assets/element1-6.png";
 
 import img1 from "../assets/element2-6.jpg";
 import img2 from "../assets/element3-6.jpg";
-import img3 from "../assets/element4-6.png";
+import img3 from "../assets/element4-6.jpg";
 import img4 from "../assets/element5-6.jpg";
 import img5 from "../assets/element6-6.jpg";
 
