c351510ceed9b9cdf46cea11ea0868bfd694dcd4
SIC_04_ELWAS <107783540+Elwass@users.noreply.github.com>
2026-02-26 16:37:18 +0700

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -7,7 +7,7 @@ export default function Navbar() {
   return (
     <nav className="w-full bg-[#FAFAFA] text-gray-900 relative">
 
-      <div className="max-w-7xl mx-auto flex items-center justify-between py-2 px-4 md:px-6">
+      <div className="max-w-7xl mx-auto flex itemskontak-center justify-between py-2 px-4 md:px-6">
 
         {/* Logo */}
         <div className="flex items-center">
@@ -106,7 +106,7 @@ export default function Navbar() {
               </li>
 
               <li>
-                <a href="#contact" onClick={() => setIsOpen(false)}>
+                <a href="https://wa.me/6288226853276" onClick={() => setIsOpen(false)}>  
                   <button className="mt-4 bg-gray-800 text-white px-6 py-2">
                     KONTAK
                   </button>
